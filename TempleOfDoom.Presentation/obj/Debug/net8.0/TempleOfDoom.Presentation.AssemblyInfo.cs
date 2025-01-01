aa4cfc41f2778b7f577be77087df3edf66fef4b0
Freek <fpw.straten@student.avans.nl>
2025-01-01 16:39:29 +0100

    removed unused item

--- TempleOfDoom.Presentation/obj/Debug/net8.0/TempleOfDoom.Presentation.AssemblyInfo.cs
+++ TempleOfDoom.Presentation/obj/Debug/net8.0/TempleOfDoom.Presentation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TempleOfDoom.Presentation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8897ff9c055205a280581d6185671224e1239811")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90c472d2d447792d2a363630e4ca9dfe058231fd")]
 [assembly: System.Reflection.AssemblyProductAttribute("TempleOfDoom.Presentation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TempleOfDoom.Presentation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
